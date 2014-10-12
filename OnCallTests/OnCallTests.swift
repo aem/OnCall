fe0bc59f80488b4ac5c860dd5b5e8fb01a4a7e5b
aem <amarkon895@gmail.com>
2014-10-12 12:26:02 -0400

    Final set up complete, ready to pretend we know what we're doing.

--- OnCallTests/OnCallTests.swift
+++ OnCallTests/OnCallTests.swift
@@ -23,7 +23,7 @@ class OnCallTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        
     }
     
     func testPerformanceExample() {
