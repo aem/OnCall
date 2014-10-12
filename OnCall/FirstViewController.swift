8a353531ad62bd7b42dde2c06410d9bad091e845
aem <amarkon895@gmail.com>
2014-10-11 23:30:53 -0400

    Initial Commit

---
+++ OnCall/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OnCall
+//
+//  Created by Adam Markon on 10/11/14.
+//  Copyright (c) 2014 Marksman Apps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
