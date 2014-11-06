ecaf5058cb3faf69a167a1df31a0578f446a30d1
nshenkman <nikita.shenkman@gmail.com>
2014-11-06 14:35:57 -0500

    figured out how to launch xib file

--- OnCall/FirstViewController.swift
+++ OnCall/FirstViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let lc = LoginViewController(nibName: "LoginViewController", bundle: nil)
         
+        self.presentViewController(lc, animated: animated, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
     
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
     
     
 }
