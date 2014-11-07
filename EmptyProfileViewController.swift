3b2d1f6602ce130c0575990c686b7cf52ece6ea5
nshenkman <nikita.shenkman@gmail.com>
2014-11-06 21:23:50 -0500

    finished sign up screen and started empty profile view controller

--- EmptyProfileViewController.swift
+++ EmptyProfileViewController.swift
@@ -1,7 +1,5 @@
-
-
 //
-//  SignUpViewController.swift
+//  EmptyProfileViewController.swift
 //  OnCall
 //
 //  Created by Nikita Shenkman on 11/6/14.
@@ -10,7 +8,7 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class EmptyProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +19,6 @@ class SignUpViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    @IBAction func return_login(sender: UIButton) {
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
 
